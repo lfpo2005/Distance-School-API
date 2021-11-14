8eac7cbe4c41feb44ade0799036738a17f24b6a7
lfpo2005 <lfpo2005@yahoo.com.br>
2021-11-14 19:17:52 -0300

    first commit

---
+++ course/src/main/java/com/ead/course/models/CourseModel.java
@@ -0,0 +1,77 @@
+package com.ead.course.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import com.ead.course.enums.CourseStatus;
+import com.ead.course.enums.CouseLevel;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+
+
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_COURSES")
+public class CourseModel implements Serializable {
+    private static final long serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID courseId;
+    
+    @Column(nullable = false, length = 150)
+    private String name;
+    
+    @Column(nullable = false, length = 250)
+    private String description;
+    
+    @Column
+    private String imageUrl;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false)    
+    private LocalDateTime creationDate;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(nullable = false)
+    private LocalDateTime lastUpdateDate;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CourseStatus courseStatus;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CouseLevel couseLevel;
+    
+    @Column(nullable = false)
+    private UUID userInstructor;
+    
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "course", fetch =  FetchType.LAZY)
+    @Fetch(FetchMode.SUBSELECT)
+    private Set<ModuleModel> modules;
+
+}
