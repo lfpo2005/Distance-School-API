7b3695d9fb7cebbece7dd9afce8f0213f5778e78
lfpo2005 <lfpo2005@yahoo.com.br>
2022-11-23 16:41:42 -0300

    merged Cross-Cutting Authentication and Authorization with JWT in Microservices

--- course/src/main/java/com/ead/course/controllers/CourseController.java
+++ course/src/main/java/com/ead/course/controllers/CourseController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,6 +33,8 @@ public class CourseController {
     CourseService courseService;
     @Autowired
     CourseValidator courseValidator;
+
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @PostMapping
     public ResponseEntity<Object> saveCourse(@RequestBody CourseDto courseDto, Errors errors){
         log.debug("POST saveCourse courseDto received: ------> {}", courseDto.toString());
@@ -48,6 +51,7 @@ public class CourseController {
         return ResponseEntity.status(HttpStatus.CREATED).body(courseService.save(courseModel));
     }
 
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @DeleteMapping("/{courseId}")
     public ResponseEntity<Object> deleteCourse(@PathVariable(value="courseId") UUID courseId) {
         log.debug("DELETE deleteCourse courseId received: ------> {}", courseId);
@@ -61,6 +65,7 @@ public class CourseController {
         return ResponseEntity.status(HttpStatus.OK).body("Course deleted successfully.");
     }
 
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @PutMapping("/{courseId}")
     public ResponseEntity<Object> updateCourse(@PathVariable(value = "courseId") UUID courseId,
                                                @RequestBody @Valid CourseDto courseDto) {
@@ -80,7 +85,7 @@ public class CourseController {
         log.info("Course updated successfully courseId: ------> {} ", courseModel.getCourseId());
         return ResponseEntity.status(HttpStatus.OK).body(courseService.save(courseModel));
     }
-
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping
     public ResponseEntity<Page<CourseModel>> getAllCourses(SpecificationTemplate.CourseSpec spec,
                                                            @PageableDefault(page = 0, size = 10,
@@ -95,6 +100,7 @@ public class CourseController {
         }
     }
 
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/{courseId}")
     public ResponseEntity<Object> getOneCourse(@PathVariable(value="courseId") UUID courseId){
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
--- course/src/main/java/com/ead/course/validation/CourseValidator.java
+++ course/src/main/java/com/ead/course/validation/CourseValidator.java
@@ -1,12 +1,14 @@
 package com.ead.course.validation;
 
+
+import com.ead.course.configs.security.AuthenticationCurrentUserService;
 import com.ead.course.dtos.CourseDto;
 import com.ead.course.enums.UserType;
 import com.ead.course.models.UserModel;
 import com.ead.course.services.UserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -24,8 +26,11 @@ public class CourseValidator  implements Validator {
     @Autowired
     UserService userService;
 
+    @Autowired
+    AuthenticationCurrentUserService authenticationCurrentUserService;
+
     @Override
-    public boolean supports(Class<?> clazz) {
+    public boolean supports(Class<?> aClass) {
         return false;
     }
 
@@ -33,18 +38,23 @@ public class CourseValidator  implements Validator {
     public void validate(Object o, Errors errors) {
         CourseDto courseDto = (CourseDto) o;
         validator.validate(courseDto, errors);
-        if(!errors.hasErrors()) {
+        if(!errors.hasErrors()){
             validateUserInstructor(courseDto.getUserInstructor(), errors);
         }
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors) {
-        Optional<UserModel> userModelOptional = userService.findById(userInstructor);
-        if (!userModelOptional.isPresent()){
-            errors.rejectValue("userInstructor", "userInstructorError", "User INSTRUCTOR not found.");
-        }
-        if (userModelOptional.get().getUserType().equals(UserType.STUDENT.toString())){
-            errors.rejectValue("userInstructor", "userInstructorError", "User must be INSTRUCTOR or ADMIN.");
+        UUID currentUserId = authenticationCurrentUserService.getCurrentUser().getUserId();
+        if(currentUserId.equals(userInstructor)) {
+            Optional<UserModel> userModelOptional = userService.findById(userInstructor);
+            if (!userModelOptional.isPresent()) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found.");
+            }
+            if (userModelOptional.get().getUserType().equals(UserType.STUDENT.toString())) {
+                errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
+            }
+        } else {
+            throw new AccessDeniedException("Forbidden");
         }
     }
 }
